e93bec94cc5c268cfce859af9c429525f8a597dc
Wim Looman <wim@nemo157.com>
2018-06-05 19:21:41 +0200

    Use map_res for parsing

--- src/parse/diag.rs
+++ src/parse/diag.rs
@@ -1,17 +1,8 @@
 use std::str::FromStr;
 
-use nom;
+use nom::digit;
 
 use {Error, Result, Simple, Value};
-
-#[inline]
-pub fn decimal<T: FromStr>(input: &str) -> nom::IResult<&str, T> {
-    let (remaining, parsed) = is_a!(input, "0123456789")?;
-    let value = parsed
-        .parse()
-        .map_err(|_| nom::Err::Failure(nom::Context::Code(input, nom::ErrorKind::Custom(0))))?;
-    Ok((remaining, value))
-}
 
 named! {
     false_<&str, Simple>,
@@ -37,7 +28,7 @@ named! {
     simple<&str, Simple>,
     do_parse!(
         tag!("simple") >>
-        value: delimited!(tag!("("), call!(decimal::<u8>), tag!(")")) >>
+        value: map_res!(delimited!(tag!("("), digit, tag!(")")), u8::from_str) >>
         (Simple(value)))
 }
 
