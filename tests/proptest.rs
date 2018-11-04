473c9371ddd2b763513b942f7d10d8f2076dbd92
Wim Looman <wim@nemo157.com>
2018-11-04 15:25:46 +0100

    Add some more generated data, fix new lines in comments

--- tests/proptest.rs
+++ tests/proptest.rs
@@ -6,14 +6,28 @@ extern crate hex;
 #[macro_use]
 extern crate pretty_assertions;
 
-use cbor_diag::{parse_bytes, parse_diag, parse_hex, DataItem, IntegerWidth};
-
+use cbor_diag::{
+    parse_bytes, parse_diag, parse_hex, ByteString, DataItem, IntegerWidth,
+    TextString,
+};
 use proptest::{
     arbitrary::any,
     collection::{self, SizeRange},
     option,
     strategy::{Just, Strategy},
 };
+use std::cmp;
+
+fn bitwidth_max(width: IntegerWidth) -> u64 {
+    match width {
+        IntegerWidth::SixtyFour => u64::max_value(),
+        IntegerWidth::ThirtyTwo => u32::max_value().into(),
+        IntegerWidth::Sixteen => u16::max_value().into(),
+        IntegerWidth::Eight => u8::max_value().into(),
+        IntegerWidth::Zero => 23,
+        IntegerWidth::Unknown => unreachable!(),
+    }
+}
 
 fn arb_integer_width() -> impl Strategy<Value = IntegerWidth> {
     prop_oneof![
@@ -25,18 +39,58 @@ fn arb_integer_width() -> impl Strategy<Value = IntegerWidth> {
     ]
 }
 
-fn arb_integer() -> impl Strategy<Value = DataItem> {
+fn arb_unsigned() -> impl Strategy<Value = (u64, IntegerWidth)> {
     arb_integer_width().prop_flat_map(|bitwidth| {
-        match bitwidth {
-            IntegerWidth::SixtyFour => (0..u64::max_value()),
-            IntegerWidth::ThirtyTwo => (0..0xffffffff),
-            IntegerWidth::Sixteen => (0..0xffff),
-            IntegerWidth::Eight => (0..0xff),
-            IntegerWidth::Zero => (0..24),
-            IntegerWidth::Unknown => unreachable!(),
-        }
-        .prop_map(move |value| DataItem::Integer { value, bitwidth })
+        (0..=bitwidth_max(bitwidth)).prop_map(move |value| (value, bitwidth))
     })
+}
+
+fn arb_integer() -> impl Strategy<Value = DataItem> {
+    arb_unsigned()
+        .prop_map(|(value, bitwidth)| DataItem::Integer { value, bitwidth })
+}
+
+fn arb_negative() -> impl Strategy<Value = DataItem> {
+    arb_unsigned()
+        .prop_map(|(value, bitwidth)| DataItem::Negative { value, bitwidth })
+}
+
+fn arb_bytestring() -> impl Strategy<Value = ByteString> {
+    arb_integer_width().prop_flat_map(|bitwidth| {
+        collection::vec(
+            any::<u8>(),
+            0..=cmp::min(bitwidth_max(bitwidth) as usize, 300),
+        )
+        .prop_map(move |data| ByteString { data, bitwidth })
+    })
+}
+
+fn arb_definite_bytestring() -> impl Strategy<Value = DataItem> {
+    arb_bytestring().prop_map(DataItem::ByteString)
+}
+
+fn arb_indefinite_bytestring() -> impl Strategy<Value = DataItem> {
+    collection::vec(arb_bytestring(), 0..10)
+        .prop_map(DataItem::IndefiniteByteString)
+}
+
+fn arb_textstring() -> impl Strategy<Value = TextString> {
+    arb_integer_width().prop_flat_map(|bitwidth| {
+        ".{0,32}"
+            .prop_filter("string too long", move |data| {
+                data.len() as u64 <= bitwidth_max(bitwidth)
+            })
+            .prop_map(move |data| TextString { data, bitwidth })
+    })
+}
+
+fn arb_definite_textstring() -> impl Strategy<Value = DataItem> {
+    arb_textstring().prop_map(DataItem::TextString)
+}
+
+fn arb_indefinite_textstring() -> impl Strategy<Value = DataItem> {
+    collection::vec(arb_textstring(), 0..10)
+        .prop_map(DataItem::IndefiniteTextString)
 }
 
 fn arb_array(
@@ -51,7 +105,14 @@ fn arb_array(
 }
 
 fn arb_data_item_leaf() -> impl Strategy<Value = DataItem> {
-    prop_oneof![arb_integer(),]
+    prop_oneof![
+        arb_integer(),
+        arb_negative(),
+        arb_definite_bytestring(),
+        arb_indefinite_bytestring(),
+        arb_definite_textstring(),
+        arb_indefinite_textstring()
+    ]
 }
 
 fn arb_data_item() -> impl Strategy<Value = DataItem> {
@@ -91,4 +152,13 @@ proptest! {
 fn multiply_overflow() {
     let _ = parse_bytes(hex::decode("7b2000000000000000").unwrap());
     let _ = parse_bytes(hex::decode("5b2000000000000000").unwrap());
+}
+
+#[test]
+fn newline_in_string() {
+    let item = DataItem::TextString(TextString {
+        data: "\n".into(),
+        bitwidth: IntegerWidth::Zero,
+    });
+    assert_eq!(item, parse_hex(item.to_hex()).unwrap());
 }
